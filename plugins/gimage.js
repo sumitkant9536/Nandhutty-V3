7bfd5f266bcc86932ff0059825cc377e8a417796
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-05-31 16:05:30 +0530

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -3,15 +3,19 @@ let _gis = require('g-i-s')
 let gis = promisify(_gis)
 let fetch = require('node-fetch')
 
-let handler = async(m, { conn, text, command, usedPrefix }) => {
-  if (!text) throw `Usage:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} banana`
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+  if (!text) throw `uhm.. what are you looking for?\n\nexample:\n${usedPrefix + command} Earth`
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
   if (!url) throw '404 Not Found'
-  conn.sendFile(m.chat, url, 'gimage', '', m, 0, { thumbnail: await (await fetch(url)).buffer() })
+  conn.sendFile(m.chat, url, 'img', '', m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
-handler.help = ['gimage <search>', 'image <search>']
+handler.help = ['gimage <search>', 'image <search>','img <search>']
 handler.tags = ['internet']
-handler.command = /^(g?image)$/i
+handler.command = /^(g?image|img)$/i
 
 module.exports = handler
+
+function pickRandom(arr) {
+  return arr[Math.floor(Math.random(5) * arr.length)]
+}
