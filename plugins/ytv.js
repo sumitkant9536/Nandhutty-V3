42bff1c2a9d4f1dd1555d53c56afc9c9a8e1e4a4
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-05-31 19:40:55 +0530

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,19 +1,18 @@
 let limit = 30
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
-
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `Use:\n${usedPrefix + command} <url>\n\nExample:\n${usedPrefix + command} https://www.youtube.com/watch?v=yxDdj_G9uRY`
-  let chat = db.data.chats[m.chat]
+  if (!args || !args[0]) throw 'uhm... wheres the url?'
+  let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   try {
     let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
     let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    m.reply(isLimit ? `Size File: ${filesizeF}\nFile size above${limit} MB, download it yourself : ${dl_link}` : wait)
+    m.reply(isLimit ? `File Size: ${filesizeF}\nFile size above ${limit} MB, download it yourself: ${dl_link}` : global.wait)
     let _thumb = {}
     try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
     catch (e) { }
-    if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
+    if (!isLimit) conn.sendFile(m.chat, dl_link, '', `
 *Title:* ${title}
 *File Size:* ${filesizeF}
   `.trim(), m, 0, {
@@ -21,13 +20,23 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
       asDocument: chat.useDocument
     })
   } catch (e) {
-    return conn.sendButton(m.chat, 'Server Error', '', 'Try again', `${usedPrefix + command} ${args[0]}`)
+    return await conn.sendButton(m.chat, 'Server Error', '', 'Try Again', `${usedPrefix + command} ${args[0]}`)
   }
 }
 handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
 handler.tags = ['downloader']
-handler.command = /^yt(v|mp4)?$/i
+handler.command = /^ytv?$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-handler.limit = 1
+handler.admin = false
+handler.botAdmin = false
 
-module.exports = handler 
+handler.fail = null
+handler.exp = 0
+handler.limit = false
+
+module.exports = handler
