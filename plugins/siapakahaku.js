f4c00113693f62bdfb38759a2225ed53b875bf23
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 02:48:39 +0530

    Update siapakahaku.js

--- plugins/siapakahaku.js
+++ plugins/siapakahaku.js
@@ -1,26 +1,30 @@
 let fetch = require('node-fetch')
+
 let timeout = 120000
 let poin = 500
-
 let handler = async (m, { conn, usedPrefix }) => {
     conn.siapakahaku = conn.siapakahaku ? conn.siapakahaku : {}
     let id = m.chat
-    if (id in conn.siapakahaku) return conn.reply(m.chat, 'Velum answered!', conn.siapakahaku[id][0])
-    let res = await fetch(API('amel', '/siapakahaku', {}, 'apikey'))
-    if (!res.ok) throw eror
+    if (id in conn.siapakahaku) {
+        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.siapakahaku[id][0])
+        throw false
+    }
+    let res = await fetch(global.API('xteam', '/game/siapakahaku', {}, 'APIKEY'))
+    if (res.status !== 200) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
     let caption = `
-${json.soal}
+Siapakah aku? ${json.result.soal}
 
-Timeout *${(timeout / 1000).toFixed(2)} second*
-Type ${usedPrefix}who for help
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}who untuk bantuan
+Bonus: ${poin} XP
 `.trim()
     conn.siapakahaku[id] = [
-        await conn.sendButton(m.chat, caption, '© ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', 'Help', '.who', m),
+        await conn.reply(m.chat, caption, '© ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', 'Help', '.who',m),
         json, poin,
         setTimeout(() => {
-            if (conn.siapakahaku[id]) conn.sendButton(m.chat, `Time is up!\nThe answer is*${json.jawaban}*`, '© ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', 'Who am I', '.siapaaku', conn.siapakahaku[id][0])
+            if (conn.siapakahaku[id]) conn.reply(m.chat, `Time is up!\nThe answer is *${json.result.jawaban}*`, conn.siapakahaku[id][0])
             delete conn.siapakahaku[id]
         }, timeout)
     ]
@@ -28,7 +32,5 @@ Type ${usedPrefix}who for help
 handler.help = ['siapakahaku']
 handler.tags = ['game']
 handler.command = /^siapa(kah)?aku/i
-
-handler.game = true
 
 module.exports = handler
