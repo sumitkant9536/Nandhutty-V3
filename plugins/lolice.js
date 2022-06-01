53185daac69e738ed9371e6588ac249198a28aa1
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 22:33:11 +0530

    Update lolice.js

--- plugins/lolice.js
+++ plugins/lolice.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/lolice', {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-  }), 'lolice.png', 'liuliuliuliuliu kami dengar disini ada lolicon', m)
+  }), 'lolice.png', 'wiiiouuuwiiiouuuwiiiouuu we heard here there is a police', m)
 }
 
 handler.help = ['lolice']
 handler.tags = ['maker']
 
-handler.command = /^(lolice)$/i
+handler.command = /^(police)$/i
 
 module.exports = handler
