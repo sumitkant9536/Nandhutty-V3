ed06bdc60dd9b7467f978f5524746d7958dce23d
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 15:33:27 +0530

    Update carigrup.js

--- plugins/carigrup.js
+++ plugins/carigrup.js
@@ -1,16 +1,18 @@
-const fetch = require('node-fetch')
-
-let handler = async (m, { text, usedPrefix, command }) => {
+let fetch = require('node-fetch')
+let handler = async (m, { conn, text }) => {
     if (!text) throw `Use:\n${usedPrefix + command} <text>\n\nExample :\n${usedPrefix + command} good luck`
-    let res = await fetch(API('amel', '/carigrup', { q: text }, 'apikey'))
-    if (!res.ok) throw `${res.status} ${res.statusText}`
+    let res = await fetch(global.API('xteam', '/search/grupwa', {
+        q: text
+    }, 'APIKEY'))
+    if (res.status !== 200) throw await res.text()
     let json = await res.json()
-    let teks = json.data.map(res => res.subject + '\n' + res.link).join('\n\n')
+    if (!json.status) throw json
+    let teks = json.result.map(res => res.subject + '\n' + res.link).join('\n\n')
     m.reply(teks)
 }
 handler.help = ['carigrup <text>']
 handler.tags = ['tools']
 
-handler.command = /^carig(ro?up|c)/i
+handler.command = /^carig(ro?up|sr)/i
 
 module.exports = handler
