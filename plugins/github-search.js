d97dba5efba17e7ec2356a724ede7aac60669c20
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 16:41:07 +0530

    Update github-search.js

--- plugins/github-search.js
+++ plugins/github-search.js
@@ -1,19 +1,18 @@
 let fetch = require('node-fetch')
-
-let handler = async(m, { text, command, usedPrefix }) => {
+let handler = async (m, { text }) => {
     if (!text) throw `Usage:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} Ajmal-Achu`
     let res = await fetch(global.API('https://api.github.com', '/search/repositories', {
         q: text
     }))
-    if (!res.ok) throw error
     let json = await res.json()
+    if (res.status !== 200) throw json
     let str = json.items.map((repo, index) => {
         return `
 ${1 + index}. *${repo.full_name}*${repo.fork ? ' (fork)' : ''}
 _${repo.html_url}_
 _Created on *${formatDate(repo.created_at)}*_
 _Last update on *${formatDate(repo.updated_at)}*_
-${repo.watchers} ${repo.forks} ${repo.stargazers_count}
+👁  ${repo.watchers}   🍴  ${repo.forks}   ⭐  ${repo.stargazers_count}
 ${repo.open_issues} Issue${repo.description ? `
 *Description:*\n${repo.description}` : ''}
 *Clone:* \`\`\`$ git clone ${repo.clone_url}\`\`\`
@@ -21,7 +20,7 @@ ${repo.open_issues} Issue${repo.description ? `
     }).join('\n\n')
     m.reply(str)
 }
-handler.help = ['githubsearch','ghsearch'].map(v => v + ' <search>')
+handler.help = ['githubsearch'].map(v => v + ' <search>')
 handler.tags = ['tools']
 
 handler.command = /^g(ithub|h)search$/i
@@ -31,12 +30,12 @@ module.exports = handler
 function formatDate(n, locale = 'id') {
     let d = new Date(n)
     return d.toLocaleDateString(locale, {
-        weekdays: 'long',
-        day: 'numeric',
-        months: 'long',
-        year: 'numeric',
-        hour: 'numeric',
-        minutes: 'numeric',
-        second: 'numeric'
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
     })
-}
+  }
