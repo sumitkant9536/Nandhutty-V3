de15c59fe5d6e6d01c5990028fa10a2a85d6a852
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 19:37:19 +0530

    Update kapankah.js

--- plugins/kapankah.js
+++ plugins/kapankah.js
@@ -1,12 +1,31 @@
-let handler = async (m, { conn }) => {
-  m.reply(`
-*Question:* ${m.text}
-*Answer:* ${Math.floor(Math.random() * 10)} ${conn.pickRandom(['second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade', 'century'])} again ...
-`.trim())
+let handler = async (m, { conn, text }) => {
+  conn.reply(m.chat, `
+*Pertanyaan:* ${m.text}
+*Jawaban:* ${Math.floor(Math.random() * 10)} ${pickRandom(['second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade', 'century'])} again ...
+`.trim(), m, m.mentionedJid ? {
+  contextInfo: {
+    mentionedJid: m.mentionedJid
+  }
+} : {})
 }
 handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <text>?')
 handler.tags = ['shell']
 handler.customPrefix = /(\?$)/
 handler.command = /^kapan(kah)?$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
-module.exports = handler 
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
