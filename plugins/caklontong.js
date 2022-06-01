e16fccbbe86cfdc8963988f00590a13806916733
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 15:26:58 +0530

    Update caklontong.js

--- plugins/caklontong.js
+++ plugins/caklontong.js
@@ -1,29 +1,29 @@
 let fetch = require('node-fetch')
+
 let timeout = 120000
 let poin = 500
-
+let src
 let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
     if (id in conn.caklontong) {
-        conn.reply(m.chat, 'belum dijawab!', conn.caklontong[id][0])
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
         throw false
     }
-    let res = await fetch(API('amel', '/caklontong', {}, 'apikey'))
-    if (!res.ok) throw eror
-    let json = await res.json()
-    if (!json.status) throw json
+    if (!src) src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
 ${json.soal}
 
-Timeout *${(timeout / 1000).toFixed(2)} seconds*
-Type ${usedPrefix}calo for help
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}calo untuk bantuan
+Bonus: ${poin} XP
 `.trim()
     conn.caklontong[id] = [
-        await conn.sendButton(m.chat, caption, wm, 'Help', '.calo', m),
+        await conn.sendButton(m.chat, caption, author, 'Bantuan', '.calo', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Time is up!\nThe answer is *${json.jawaban}*\n${json.deskripsi}`, wm, 'Cak Lontong', '.caklontong', conn.caklontong[id][0])
+            if (conn.caklontong[id]) conn.sendButton(m.chat, `Time is up!\nThe answer is *${json.jawaban}*\n${json.deskripsi}`, author, 'Cak Lontong', '.caklontong', conn.caklontong[id][0])
             delete conn.caklontong[id]
         }, timeout)
     ]
@@ -31,7 +31,5 @@ Type ${usedPrefix}calo for help
 handler.help = ['caklontong']
 handler.tags = ['game']
 handler.command = /^caklontong/i
-
-handler.game = true
 
 module.exports = handler
