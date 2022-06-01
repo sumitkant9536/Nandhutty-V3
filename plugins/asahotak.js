f053e5c2a924e0dcead28d26e64498a25b94fb4b
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 14:41:12 +0530

    Update asahotak.js

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -1,7 +1,7 @@
-const fetch = require('node-fetch')
+let fetch = require('node-fetch')
+
 let timeout = 120000
 let poin = 500
-
 let handler = async (m, { conn, usedPrefix }) => {
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
@@ -9,29 +9,28 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'not answered!', conn.asahotak[id][0])
         throw false
     }
-    let res = await fetch(API('amel', '/asahotak', {}, 'apikey'))
-    if (!res.ok) throw eror
+    let res = await fetch(global.API('xteam', '/game/asahotak', {}, 'APIKEY'))
+    if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
     let caption = `
-${json.soal}
+${json.result.soal}
 
-Timeout *${(timeout / 1000).toFixed(2)} seconds*
-Type ${usedPrefix}ao for help
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}ao untuk bantuan
+Bonus: ${poin} XP
 `.trim()
     conn.asahotak[id] = [
         await conn.sendButton(m.chat, caption, wm, 'Help', '.ao', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.asahotak[id]) await conn.sendButton(m.chat, `Time is up!\nThe answer is *${json.jawaban}*`, wm, 'Asah Otak', '.asahotak', conn.asahotak[id][0])
+            if (conn.asahotak[id]) await conn.sendButton(m.chat, `Time up!\nThe answer is *${json.result.jawaban}*`, wm, 'Asah Otak', '.asahotak', conn.asahotak[id][0])
             delete conn.asahotak[id]
         }, timeout)
     ]
 }
-handler.help = ['use brain']
+handler.help = ['asahotak']
 handler.tags = ['game']
 handler.command = /^usebrain/i
-
-handler.game = true
 
 module.exports = handler
