984e2a9736e6e079e4da385bb2aea622de31ac81
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 02:55:58 +0530

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,14 +1,13 @@
 let fetch = require('node-fetch')
-
-let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `Use :\n${usedPrefix + command} <text>\n\nExample :\n${usedPrefix + command} hai`
-  let res = await fetch(API('https://api.simsimi.net', '/v2/', { text, lc: m }))
-  if (!res.ok) throw eror
+let handler = async (m, { text }) => {
+  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "id" }, ''))
   let json = await res.json()
-  await m.reply(`*Simi:* ${json.success}`)
+  if (json.success) m.reply(json.success)
+  else throw json
 }
-handler.help = ['simi', 'simsimi'].map(v => v + ' <text>')
+handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 
-module.exports = handler 
+module.exports = handler
+
