640d6917bb3cfc5c843afb3295039f332977d263
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 16:34:52 +0530

    Update getsider.js

--- plugins/getsider.js
+++ plugins/getsider.js
@@ -1,16 +1,16 @@
-let handler = async(m, { conn, command, usedPrefix }) => {
-  if (!m.quoted) throw `Reply to bot messages with the command *${usedPrefix + command}*`
+let handler = async (m, { conn }) => {
+  if (!m.quoted) throw 'Reply to bot messages with the command *${usedPrefix + command}*`
   if (!m.quoted.fromMe) throw false
   if (!m.quoted.id) throw false
   let members = m.quoted.chat.endsWith('g.us') ? (await conn.groupMetadata(m.quoted.chat)).participants.length - 1 : m.quoted.chat.endsWith('@broadcast') ? -1 : 1
   let { reads, deliveries } = await conn.messageInfo(m.quoted.chat, m.quoted.id)
   let txt = `
 *Read by:*
-${reads.sort((a, b) => bt - at).map(({ jid, t }) => `@${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
+${reads.sort((a, b) => b.t - a.t).map(({ jid, t }) => `@${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
 ${members > 1 ? `${members - reads.length} remaining` : ''}
 
-*Sent to:*
-${deliveries.sort((a, b) => bt - at).map((({ jid, t }) => `wa.me/${jid.split`@`[0]}\n_${ formatDate(t * 1000)}_`).join('\n'))}
+*Delivered to:*
+${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `wa.me/${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
 ${members > 1 ? `${members - reads.length - deliveries.length} remaining` : ''}
 `.trim()
   m.reply(txt, null, {
@@ -21,6 +21,7 @@ ${members > 1 ? `${members - reads.length - deliveries.length} remaining` : ''}
 }
 handler.help = ['getsider']
 handler.tags = ['group']
+
 handler.command = /^getsider$/
 
 module.exports = handler
