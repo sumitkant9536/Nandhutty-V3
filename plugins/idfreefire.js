1271ecd6c6ba69e7e8027a85e3fc555b5d91039d
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 12:31:18 +0530

    Update idfreefire.js

--- plugins/idfreefire.js
+++ plugins/idfreefire.js
@@ -1,12 +1,9 @@
 let fetch = require('node-fetch')
-
-let handler = async(m, { args, usedPrefix, command }) => {
-  if (!args[0]) throw `Usage:\n${usedPrefix + command} <number>\n\nExample:\n${usedPrefix + command} 4404748333`
+let handler = async (m, { args }) => {
   let res = await fetch(global.API('xteam', '/search/freefire', { id: args[0] }, 'APIKEY'))
-  if (!res.ok) throw error
   let json = await res.json()
-  if (!json.status) throw json
-  m.reply(json.result.name)
+  if (json.result.name) m.reply(`\nNama : ${json.result.name}\nID : ${args[0]}\n`)
+  else throw `Error!\n\n${json}`
 }
 handler.help = ['epep'].map(v => v + ' <id>')
 handler.tags = ['internet']
