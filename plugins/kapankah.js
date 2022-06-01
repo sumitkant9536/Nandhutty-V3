425811000d7e129765cfae1a3fca513b9cf2c1ab
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 19:47:20 +0530

    Update kapankah.js

--- plugins/kapankah.js
+++ plugins/kapankah.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${Math.floor(Math.random() * 10)} ${pickRandom(['second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade', 'century'])} again ...
+*Question:* ${m.text}
+*Answer:* ${Math.floor(Math.random() * 10)} ${pickRandom(['second', 'minute', 'hour', 'day', 'week', 'month', 'year', 'decade', 'century'])} again ...
 `.trim(), m, m.mentionedJid ? {
   contextInfo: {
     mentionedJid: m.mentionedJid
@@ -11,7 +11,7 @@ let handler = async (m, { conn, text }) => {
 handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <text>?')
 handler.tags = ['shell']
 handler.customPrefix = /(\?$)/
-handler.command = /^kapan(kah)?$/i
+handler.command = /^(when)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
