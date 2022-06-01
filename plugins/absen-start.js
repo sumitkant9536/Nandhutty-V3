4d2d09973894329572a82f10163b081e2d4ac6d3
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-01 11:51:52 +0530

    Update absen-start.js

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -1,18 +1,18 @@
-let handler = async (m, { conn, usedPrefix, text }) => {
+let handler = async (m, { conn, usedPrefix, text, isAdmin, isOwner }) => {
     if (m.isGroup) {
         if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     }
     conn.absen = conn.absen ? conn.absen : {}
     let id = m.chat
-    if (id in conn.absen) return await conn.sendButton(m.chat, `There are still absences in this chat!`, 'watermark', 'Delete', `${usedPrefix}-absence`, conn.absen[id][0])
+    if (id in conn.absen) return conn.send2Button(m.chat, `Masih ada absen di chat ini!`, '© stikerin', 'Hapus', `${usedPrefix}hapusabsen`, 'Cek', `${usedPrefix}cekabsen`, conn.absen[id][0])
     conn.absen[id] = [
-        await conn.sendButton(m.chat, `Absence begins`, 'watermark', 'Absence', `${usedPrefix}absence`, m),
+        await conn.sendButton(m.chat, `Absen dimulai`, '© wabot-aq', 'Absen', `${usedPrefix}absen`, m),
         [],
         text
     ]
 }
-handler.help = ['mulaiabsen [text]']
+handler.help = ['mulaiabsen [teks]']
 handler.tags = ['absen']
-handler.command = /^(\+|start|mulai)absen$/i
+handler.command = /^(start|mulai|\+)absen$/i
 
 module.exports = handler
