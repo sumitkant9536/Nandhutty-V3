4456440a3e2dcc470d547d1507da12ed195e9455
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 22:07:10 +0530

    Update tebaklirik_ans.js

--- plugins/tebaklirik_ans.js
+++ plugins/tebaklirik_ans.js
@@ -3,19 +3,19 @@ const threshold = 0.72
 module.exports = {
     async before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teli/i.test(m.quoted.text)) return !0
-    this.tebaklirik = this.tebaklirik ? this.tebaklirik : {}
-    if (!(id in this.tebaklirik)) return m.reply('Soal itu telah berakhir')
-    if (m.quoted.id == this.tebaklirik[id][0].id) {
-        let json = JSON.parse(JSON.stringify(this.tebaklirik[id][1]))
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*lyhint/i.test(m.quoted.text)) return !0
+    this.guesslyrics = this.guesslyrics ? this.guesslyrics : {}
+    if (!(id in this.guesslyrics)) return m.reply('The matter has ended')
+    if (m.quoted.id == this.guesslyrics[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.guesslyrics[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tebaklirik[id][2]
-            m.reply(`*Benar!*\n+${this.tebaklirik[id][2]} XP`)
-            clearTimeout(this.tebaklirik[id][3])
-            delete this.tebaklirik[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+            global.db.data.users[m.sender].exp += this.guesslyrics[id][2]
+            m.reply(`*Correct!*\n+${this.guesslyrics[id][2]} XP`)
+            clearTimeout(this.guesslyrics[id][3])
+            delete this.guesslyrics[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*a little more!*`)
+        else m.reply(`*Wrong!*`)
     }
     return !0
 },
