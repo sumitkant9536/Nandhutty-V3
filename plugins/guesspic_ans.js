af08c46da8e77891f543715ff00d1e49f4e68f2f
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 12:44:42 +0530

    Update and rename tebakgambar_ans.js to guesspic_ans.js

--- plugins/guesspic_ans.js
+++ plugins/guesspic_ans.js
@@ -4,16 +4,16 @@ module.exports = {
   async before(m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/type.*hint/i.test(m.quoted.text)) return !0
-    this.tebakgambar = this.tebakgambar ? this.tebakgambar : {}
-    if (!(id in this.tebakgambar)) return m.reply('The matter has ended')
-    if (m.quoted.id == this.tebakgambar[id][0].id) {
-      let json = JSON.parse(JSON.stringify(this.tebakgambar[id][1]))
+    this.guesspic = this.guesspic ? this.guesspic : {}
+    if (!(id in this.guesspic)) return m.reply('The matter has ended')
+    if (m.quoted.id == this.guesspic[id][0].id) {
+      let json = JSON.parse(JSON.stringify(this.guesspic[id][1]))
       // m.reply(JSON.stringify(json, null, '\t'))
       if (m.text.toLowerCase() == json.result.jawaban.toLowerCase().trim()) {
-        global.db.data.users[m.sender].exp += this.tebakgambar[id][2]
-        m.reply(`*Benar!*\n+${this.tebakgambar[id][2]} XP`)
-        clearTimeout(this.tebakgambar[id][3])
-        delete this.tebakgambar[id]
+        global.db.data.users[m.sender].exp += this.guesspic[id][2]
+        m.reply(`*Correct!*\n+${this.guesspic[id][2]} XP`)
+        clearTimeout(this.guesspic[id][3])
+        delete this.guesspic[id]
       } else if (similarity(m.text.toLowerCase(), json.result.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
       else m.reply(`*Wrong!*`)
     }
