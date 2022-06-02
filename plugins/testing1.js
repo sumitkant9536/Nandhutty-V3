d32b2f840c9fda2c7dae116561fe5f3c99b04213
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 00:24:37 +0530

    Create testing1.js

---
+++ plugins/testing1.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.testing1 = conn.testing1 ? conn.testing1 : {}
+    let id = m.chat
+    if (id in conn.testing1) {
+        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.testing1[id][0])
+        throw false
+    }
+    let res = await fetch('https://raw.githubusercontent.com/sumitkant9536/database/master/games/tebakbendera2.json')
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let data = await res.json()
+    if (res.status != 200) throw await res.text()
+        let img = await res.buffer()
+        conn.sendFile(m.chat, img, '', '*© stikerin*', m, false, { thumbnail: Buffer.alloc(0) })
+    let json = data[Math.floor(Math.random() * data.length)]
+    let caption = `
+
+Timeout *${(timeout / 1000).toFixed(2)} second*
+Type ${usedPrefix}fmhint for help
+Bonus: ${poin} XP
+    `.trim()
+    conn.testing1[id] = [
+        await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.testing1[id]) conn.reply(m.chat, `Time is up!\nThe Answer Is *${json.jawaban}*`, conn.testing1[id][0])
+            delete conn.testing1[id]
+        }, timeout)
+    ]
+}
+handler.help = ['testing1']
+handler.tags = ['game']
+handler.command = /^testing1/i
+
+module.exports = handler
