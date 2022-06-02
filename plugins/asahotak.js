6724a8380e07532d1f30c80d50eff433cf12ef43
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 19:40:58 +0530

    Update asahotak.js

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -6,31 +6,31 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.asahotak = conn.asahotak ? conn.asahotak : {}
     let id = m.chat
     if (id in conn.asahotak) {
-        conn.reply(m.chat, 'not answered!', conn.asahotak[id][0])
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.asahotak[id][0])
         throw false
     }
-    let res = await fetch('https://raw.githubusercontent.com/sumitkant9536/database/master/games/asahotak.json')
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
-    let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
+    let res = await fetch(global.API('xteam', '/game/asahotak', {}, 'APIKEY'))
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.status) throw json
     let caption = `
-${json.soal}
+${json.result.soal}
 
-Timeout *${(timeout / 1000).toFixed(2)} second*
-Type ${usedPrefix}oo for help
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}ao untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.asahotak[id] = [
-        await conn.sendButton(m.chat, caption, author, 'Help', '.ao', m),
+        await conn.sendButton(m.chat, caption, author, 'Bantuan', '.ao', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.asahotak[id]) await conn.sendButton(m.chat, `Time up!\nThe answer is *${json.result.jawaban}*`, author, 'Asah Otak', '.asahotak', conn.asahotak[id][0])
+            if (conn.asahotak[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.result.jawaban}*`, author, 'Asah Otak', '.asahotak', conn.asahotak[id][0])
             delete conn.asahotak[id]
         }, timeout)
     ]
 }
 handler.help = ['asahotak']
 handler.tags = ['game']
-handler.command = /^usebrain/i
+handler.command = /^asahotak/i
 
 module.exports = handler
