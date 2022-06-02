ae457be927236f707ceb74ad1e0bdd201c1d84bc
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 11:58:11 +0530

    Update simcard.js

--- plugins/simcard.js
+++ plugins/simcard.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/simpcard', {
     avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/755a7ce9b80c608ec22d0.jpg'),
-  }), 'simpcard.png', 'simp', m)
+  }), 'simpcard.jpg', 'simp', m)
 }
 
 handler.help = ['simpcard']
