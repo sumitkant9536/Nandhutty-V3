7029d5c586753d12f7c7f42113183dc2ece8c06e
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 22:34:48 +0530

    Update and rename guessdis_ans.js to guessword_ans.js

---
+++ plugins/guessword_ans.js
@@ -0,0 +1,23 @@
+const similarity = require('similarity')
+const threshold = 0.72
+module.exports = {
+    async before(m) {
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Type.*wrhint/i.test(m.quoted.text)) return !0
+    this.guessword = this.guessword ? this.guessword : {}
+        if (!(id in this.guessword)) return m.reply('The matter has ended')
+        if (m.quoted.id == this.guessword[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.guessword[id][1]))
+        // m.reply(JSON.stringify(json, null, '\t'))
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
+            global.db.data.users[m.sender].exp += this.guessword[id][2]
+            m.reply(`*Correct!*\n+${this.guessword[id][2]} XP`)
+            clearTimeout(this.guessword[id][3])
+            delete this.guessword[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*a little more!*`)
+        else m.reply(`*Wrong!*`)
+    }
+    return !0
+},
+exp: 0
+}
