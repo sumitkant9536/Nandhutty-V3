024038d8c824662e77ae160156b64c4deb630e4d
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 13:06:56 +0530

    Create guessdis_hint.js

---
+++ plugins/guessdis_hint.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+    conn.guessdis = conn.guessdis ? conn.guessdis : {}
+    let id = m.chat
+    if (!(id in conn.guessdis)) throw false
+    let json = conn.guessdis[id][1]
+    let ans = json.result.jawaban.trim()
+    let clue = ans.replace(/[AIUEO]/g, '_')
+    m.reply('```' + clue + '```')
+}
+handler.command = /^puzzle$/i
+handler.limit = true
+module.exports = handler
