b418281a9f2cc5d68ff57c366bff251c43754740
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 19:48:07 +0530

    Update asahotak.js

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.asahotak[id][0])
         throw false
     }
-    let res = await fetch(global.API('xteam', '/game/asahotak', {}, 'APIKEY'))
+    let res = await fetch('https://raw.githubusercontent.com/sumitkant9536/database/master/games/asahotak.json')
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
