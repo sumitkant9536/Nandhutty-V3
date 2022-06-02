f97a8959c1ce17dfc4b1d625b29c56022c9341b0
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-02 21:32:02 +0530

    Update tebaklirik_hint.js

--- plugins/tebaklirik_hint.js
+++ plugins/tebaklirik_hint.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn }) => {
-    conn.tebakirik = conn.tebakirik ? conn.tebakirik : {}
+    conn.tebaklirik = conn.tebaklirik ? conn.tebaklirik : {}
     let id = m.chat
-    if (!(id in conn.tebakirik)) throw false
-    let json = conn.tebakirik[id][1]
+    if (!(id in conn.tebaklirik)) throw false
+    let json = conn.tebaklirik[id][1]
     m.reply('```' + json.jawaban.replace(/[AUIEOaiueo]/g, '_') + '```')
 }
 handler.command = /^teli$/i
