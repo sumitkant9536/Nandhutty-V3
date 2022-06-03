078d84f8a38229a7cc5a102fcea43d374553719c
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 12:35:40 +0530

    Update guessword_hint.js

--- plugins/guessword_hint.js
+++ plugins/guessword_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.guessword)) throw false
     let json = conn.guessword[id][1]
-    m.reply('```' + json.jawaban.replace(/[AUIEOaiueo]/g, '_') + '```')
+    m.reply('```' + json.answer.replace(/[AUIEOaiueo]/g, '_') + '```')
 }
 handler.command = /^wrhint$/i
 handler.limit = true
