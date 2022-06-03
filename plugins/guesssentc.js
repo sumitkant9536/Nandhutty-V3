a37205d57eeae9a4d912e777c4028b6fb1a3a08d
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 15:29:06 +0530

    Create guesssentc.js

---
+++ plugins/guesssentc.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 500
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.guesssentc = conn.guesssentc ? conn.guesssentc : {}
+    let id = m.chat
+    if (id in conn.guesssentc) {
+        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.guesssentc[id][0])
+        throw false
+    }
+    let res = await fetch('https://raw.githubusercontent.com/sumitkant9536/database/master/games/guesssentc.json')
+    if (!res.ok) throw await `${res.status} ${res.statusText}`
+    let data = await res.json()
+    let json = data[Math.floor(Math.random() * data.length)]
+    let caption = `
+*Question-* ${json.question}
+
+Timeout *${(timeout / 1000).toFixed(2)} second*
+Type ${usedPrefix}snhint for help
+Bonus: ${poin} XP
+    `.trim()
+    conn.guesssentc[id] = [
+        await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.guesssentc[id]) conn.reply(m.chat, `Time is up!\nThe Answer Is *${json.answer}*`, conn.guesssentc[id][0])
+            delete conn.guesssentc[id]
+        }, timeout)
+    ]
+}
+handler.help = ['guesssentc']
+handler.tags = ['game']
+handler.command = /^guesssentc/i
+
+module.exports = handler
