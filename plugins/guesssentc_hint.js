ae07594815a73621a31e1f6d12d30d788f718330
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 15:32:03 +0530

    Create guesssentc_hint.js

---
+++ plugins/guesssentc_hint.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn }) => {
+    conn.guesssentc = conn.guesssentc ? conn.guesssentc : {}
+    let id = m.chat
+    if (!(id in conn.guesssentc)) throw false
+    let json = conn.guesssentc[id][1]
+    m.reply('```' + json.snhint.replace(/[AUIEOaiueo]/g, '_') + '```')
+}
+handler.command = /^snhint$/i
+handler.limit = true
+module.exports = handler
