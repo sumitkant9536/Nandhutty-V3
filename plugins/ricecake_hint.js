65cc0e3efc654a7b05691dc08a0bbd494876c7df
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 13:08:52 +0530

    Update ricecake_hint.js

--- plugins/ricecake_hint.js
+++ plugins/ricecake_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.ricecake)) throw false
     let json = conn.ricecake[id][1]
-    m.reply('```' + json.jawaban.replace(/[AUIEOaiueo]/g, '_') + '```')
+    m.reply('```' + json.answer.replace(/[AUIEOaiueo]/g, '_') + '```')
 }
 handler.command = /^rchint$/i
 handler.limit = true
