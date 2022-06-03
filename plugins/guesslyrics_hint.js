f49d75d666b03ab482e82f76ae0662163406dc22
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 13:02:28 +0530

    Update guesslyrics_hint.js

--- plugins/guesslyrics_hint.js
+++ plugins/guesslyrics_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.guesslyrics)) throw false
     let json = conn.guesslyrics[id][1]
-    m.reply('```' + json.jawaban.replace(/[AUIEOaiueo]/g, '_') + '```')
+    m.reply('```' + json.answer.replace(/[AUIEOaiueo]/g, '_') + '```')
 }
 handler.command = /^lyhint$/i
 handler.limit = true
