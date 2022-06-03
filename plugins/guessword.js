504d368e34e6a6e52ecf9cbc48a4b10d34fb1d6d
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 12:32:48 +0530

    Update guessword.js

--- plugins/guessword.js
+++ plugins/guessword.js
@@ -10,12 +10,12 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.guessword[id][0])
         throw false
     }
-    let res = await fetch('https://raw.githubusercontent.com/sumitkant9536/database/master/games/tebakkata.json')
+    let res = await fetch('https://raw.githubusercontent.com/sumitkant9536/database/master/games/guessword.json')
     if (!res.ok) throw await `${res.status} ${res.statusText}`
     let data = await res.json()
     let json = data[Math.floor(Math.random() * data.length)]
     let caption = `
-${json.soal}
+*Question* ${json.question}
 
 Timeout *${(timeout / 1000).toFixed(2)} second*
 Type ${usedPrefix}wrhint for help
@@ -25,7 +25,7 @@ Bonus: ${poin} XP
         await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.guessword[id]) conn.reply(m.chat, `Time is up!\nThe Answer Is *${json.jawaban}*`, conn.guessword[id][0])
+            if (conn.guessword[id]) conn.reply(m.chat, `Time is up!\nThe Answer Is *${json.answer}*`, conn.guessword[id][0])
             delete conn.guessword[id]
         }, timeout)
     ]
