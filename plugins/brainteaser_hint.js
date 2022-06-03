a87b0102ca50b7a2e65826bbb23975e466d23bca
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 12:55:50 +0530

    Update brainteaser_hint.js

--- plugins/brainteaser_hint.js
+++ plugins/brainteaser_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.brainteaser)) throw false
     let json = conn.brainteaser[id][1]
-    m.reply('```' + json.jawaban.replace(/[AUIEOaiueo]/g, '_') + '```')
+    m.reply('```' + json.answer.replace(/[AUIEOaiueo]/g, '_') + '```')
 }
 handler.command = /^brhint$/i
 handler.limit = true
