fb4a75d20196c3cb635350505a08c679ed44ec38
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-03 13:13:32 +0530

    Update whoami_hint.js

--- plugins/whoami_hint.js
+++ plugins/whoami_hint.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let id = m.chat
     if (!(id in conn.whoami)) throw false
     let json = conn.whoami[id][1]
-    m.reply('```' + json.jawaban.replace(/[AUIEOaiueo]/g, '_') + '```')
+    m.reply('```' + json.answer.replace(/[AUIEOaiueo]/g, '_') + '```')
 }
 handler.command = /^whhint$/i
 handler.limit = true
