9ad6e397906b247b4a683f7383867ee756371a77
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-05 19:26:00 +0530

    Update tod.js

--- plugins/tod.js
+++ plugins/tod.js
@@ -1,26 +1,28 @@
-let fetch = require('node-fetch')
+let handler = async (m, { conn, usedPrefix, command }) => {
+try{let mem  = JSON.parse(fs.readFileSync('./src/tod.json'))
+await conn.sendTB2(m.chat, `${pickRandom(mem)}`, wm,null,null, 'DARE', `.dare` , 'TRUTH',`.truth` )
+}catch(e){
+  conn.reply(m.chat,`${e}`)
+  conn.reply('120363022211098165@g.us',`𝗨𝗵𝗼𝗵! 𝗮𝗻 𝗲𝗿𝗿𝗼𝗿 𝗢𝗰𝗰𝘂𝗿𝗲𝗱 
 
-let handler = async (m, { conn, command, usedPrefix }) => {
-  if (/^tod$/i.test(command)) {
-    await conn.send3Button(m.chat, 'Truth or Dare', 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, 'Acak', `${pickRandom([`${usedPrefix}dare`, `${usedPrefix}truth`])}`, m)
-  }
-  if (/^truth$/i.test(command)) {
-    let res = await fetch(API('amel', '/truth', {}, 'apikey'))
-    if (!res.ok) throw eror
-    let json = await res.json()
-    if (!json.status) throw json
-    conn.send2Button(m.chat, json.result, 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, m)
-  }
-  if (/^dare$/i.test(command)) {
-    let res = await fetch(API('amel', '/dare', {}, 'apikey'))
-    if (!res.ok) throw eror it
-    let json = await res.json()
-    if (!json.status) throw json
-    conn.send2Button(m.chat, json.result, 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', 'Truth', `${usedPrefix}truth`, 'Dare', `${usedPrefix}dare`, m)
-  }
-}
-handler.help = ['tod']
-handler.tags = ['fun']
-handler.command = /^(tod|truth|dare)$/i
+𝗘𝗿𝗿𝗼𝗿 : ${util.format(e)}
 
-module.exports = handler 
+  𝗖𝗼𝗺𝗺𝗮𝗻𝗱 : ${usedPrefix+command}
+  
+  𝗣𝗼𝘀𝘀𝗶𝗯𝗹𝗲 𝗥𝗲𝗮𝘀𝗼𝗻𝘀 :
+     • 𝗜𝗻𝘃𝗮𝗹𝗶𝗱 𝗨𝘀𝗮𝗴𝗲 𝗢𝗳 𝗖𝗼𝗺𝗺𝗮𝗻𝗱
+     • 𝗦𝗲𝗿𝘃𝗲𝗿 𝗘𝗿𝗿𝗼𝗿
+     • 𝗥𝘂𝗻𝘁𝗶𝗺𝗲 𝗘𝗿𝗿𝗼𝗿𝘀
+     • 𝗘𝗿𝗿𝗼𝗿 𝗮𝘁 𝗱𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿𝘀 𝗘𝗻𝗱
+     • 𝗗𝗮𝘁𝗮 𝗡𝗲𝘁𝘄𝗼𝗿𝗸 𝗜𝘀𝘀𝘂𝗲𝘀 `, null, {})
+}}
+
+  handler.help = ['ᴛᴏᴅ (ᴛʀᴜᴛʜ ᴏʀ ᴅᴀʀᴇ)']
+  handler.tags = ['fun']
+  handler.command = /^(tod)$/i
+
+  module.exports = handler
+
+  function pickRandom(list) {
+    return list[Math.floor(list.length * Math.random())]
+  }
