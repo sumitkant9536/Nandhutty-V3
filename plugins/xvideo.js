f2b8d8314031db7d9ec9a17f1480dc0f46f7a2a4
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-06 22:30:19 +0530

    Update xvideo.js

--- plugins/xvideo.js
+++ plugins/xvideo.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text, command, usedPrefix }) => {
     if (!text) throw `Harap masukan Judulnya \n\n\nContoh : .xvideosearch ngwe sama elaina`
-    let res = await fetch(`http://kocakz.herokuapp.com/api/media/xvideo/search?query=${text}`)
+    let res = await fetch(`http://kocakz.herokuapp.com/api/media/xhamaster/search?query=${text}`)
     if (!res.ok) throw await res.text()
     let json = await res.json()
     let keqing = json.result.map((v, i) => `#${i + 1}. \n*Judul:* ${v.title}\n*Info:* ${v.info}\n*Link:* ${v.link}}\n==============\n`).join('\n') 
@@ -11,7 +11,7 @@ let handler = async(m, { conn, text, command, usedPrefix }) => {
 handler.help = ['xvideosearch <judul>']
 handler.tags = ['dewasa']
 handler.command = /^(xvideosearch)$/i
-handler.private = true
-handler.limit = 8
+handler.private = false
+handler.limit = 1
 
 module.exports = handler
