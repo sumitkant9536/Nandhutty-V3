b5eb8bd930cf50ab483427e74f70741e410efd17
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-06 23:00:40 +0530

    Update cosplay.js

--- plugins/cosplay.js
+++ plugins/cosplay.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
      m.reply('tunggu...')
   let res = await (await fetch('https://raw.githubusercontent.com/Luigmntng/RESTAPI/master/data/cosplay.json')).json()
   let cosser = res[Math.floor(Math.random() * res.length)]
-  await conn.sendButtonImg(m.chat, cosser, 'ini 3d ?', 'By Lui', 'Next', `.cosplay`, m, false)
+  await conn.sendButtonImg(m.chat, cosser, 'ini 3d ?', 'By Lui', 'Next', '.cosplay', m, false)
 }
 handler.help = ['cosplay']
 handler.tags = ['anime']
