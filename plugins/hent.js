af354187a1aa8feea5a7a0176cad11b2716405d2
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-06 23:02:53 +0530

    Update hent.js

--- plugins/hent.js
+++ plugins/hent.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
      m.reply('tunggu...')
-  let res = await fetch(`https://hmtai.herokuapp.com/nsfw/${command}`)
+  let res = await fetch('https://hmtai.herokuapp.com/nsfw/${command}')
   let json = await res.json()
   if (!json.url) throw json
-  await conn.sendButtonImg(m.chat, json.url, 'sagne sama kartun telanjang?', `random ${command}`, 'Lagi banh', `${usedPrefix + command}`, m, false)
+  await conn.sendButtonImg(m.chat, json.url, 'sagne sama kartun telanjang?', 'random ${command}', 'Lagi banh', '${usedPrefix + command}', m, false)
 }
 handler.help = ['ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri' ]
 handler.tags = ['dewasa']
