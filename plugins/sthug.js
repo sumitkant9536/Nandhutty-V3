29e7da068f1b54dd7d94d4c883e76707e4fd47ae
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-07 00:03:30 +0530

    Update sthug.js

--- plugins/sthug.js
+++ plugins/sthug.js
@@ -3,7 +3,7 @@ const { sticker } = require('../lib/sticker')
 const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async(m, { conn }) => {
-  let res = await fetch(global.API('https://api.waifu.pics', '/sfw/hug'))
+  let res = await fetch(global.API('https://api.waifu.pics', '/sfw'))
   let json = await res.json()
   let stiker = await sticker(null, json.url, global.packname, global.author)
   if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
@@ -11,7 +11,7 @@ let handler = async(m, { conn }) => {
 }
 handler.help = ['stickerhug']
 handler.tags = ['sticker']
-handler.command = /^sthug|stickerhug|stikerhug$/i
+handler.command = /^sthug|stickerhug|kiss|stikerhug$/i
 handler.limit = true
 
 module.exports = handler
