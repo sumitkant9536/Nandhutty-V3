c8fbf59f9e814d2dbc92758273a953419c6ce239
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-06 17:53:15 +0530

    Update ass.js

--- plugins/ass.js
+++ plugins/ass.js
@@ -1,5 +1,5 @@
 let handler = async(m, { conn }) => {
-  await conn.sendFile(m.chat, pickRandom(ass), 'ass.mp4', '', m)
+  await conn.sendFile(m.chat, pickRandom(ass), 'ass.gif', '', m)
 }
 handler.help = ['ass']
 handler.tags = ['nsfw']
