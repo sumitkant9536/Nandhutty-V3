ab88eb521c6f0ac9b3c3361e247dea7328ec378e
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-07 17:32:35 +0530

    Update hent.js

--- plugins/hent.js
+++ plugins/hent.js
@@ -5,13 +5,13 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 	if (!res.ok) throw `${res.status} ${res.statusText}`
 	let json = await res.json()
 	if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'sgne sama kartun ?', m, 0, { thumbnail: Buffer.alloc(0) })
+  conn.sendFile(m.chat, json.url, '', 'like anime ?', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri' ]
 handler.tags = ['nsfw']
 handler.command = /^(ass|bdsm|blowjob|boobjob|cum|creampie|cuckold|ero|elves|femdom|foot|gangbang|glasses|hentai|incest|masturbation|pantsu|orgy|tentacles|thighs|uniform|vagina|yuri)$/i
 
-handler.premium = true
+handler.premium = false
 handler.register = true
 
 module.exports = handler
