f72d201c5feddec0788a64e39cee10eb6accc76e
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-07 17:01:58 +0530

    Update enphoto360.js

--- plugins/enphoto360.js
+++ plugins/enphoto360.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let split = '|'
 let handler = async (m, { conn, args: [effect], text: txt }) => {
-  let { effects } = await (await (fetch(global.API('lolhuman', '/ephoto')))).json()
+  let { effects } = await (await (fetch(global.API('xteam', '/ephoto')))).json()
   if (!effect) throw '*List Effect*\n\n' + effects.sort((a, b) => a - b).join('\n')
   effect = effect.toLowerCase()
   if (!effect in effects) throw `Effect *${effect}* not found`
   let [text, text2, ...text3] = txt.replace(effect, '').trimStart().split(split)
   text3 = text3.join(split)
-  let url = global.API('lolhuman', '/ephoto/' + effect, { text, text2, text3 }, 'APIKEY')
+  let url = global.API('xteam', '/ephoto/' + effect, { text, text2, text3 }, 'APIKEY')
   await conn.sendFile(m.chat, url, 'ephoto.jpg', `*ENPHOTO360*\n*Effect:* ${effect}`, m)
 }
 handler.help = ['enphoto'].map(v => v + ' <effect> <text>|[text2]|[text3]')
