141d4dfade83a5ee506f0cbe9bc11b1f695fbb8f
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-07 19:34:55 +0530

    Update jj.js

--- plugins/jj.js
+++ plugins/jj.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command }) => {
   let res = await fetch(`https://api.kaysa.xyz/api/${command}?apikey=KaysaS`)
-  if (!res.ok) throw 'Error Website sedang down'
+  if (!res.ok) throw 'Error Website server down'
   let json = await res.json()
   if (!json.video) throw 'Error!'
-   conn.sendFile(m.chat, json.video, 'meme.mp4', `Nih jedag jedug bikin sakit matanya`, m)
+   conn.sendFile(m.chat, json.video, 'meme.mp4', `Global Random`, m)
 }
 handler.help = ['jjfreefire', 'jjml', 'jjpubg', 'jjanime']
-handler.tags = ['jj']
+handler.tags = ['fun']
 handler.command = /^(jjfreefire|jjml|jjpubg|jjanime)$/i
 
 handler.limit = true
