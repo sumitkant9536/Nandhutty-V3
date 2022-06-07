038b99c05f9358a68bf84137072b068c7b6de4f0
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-07 14:20:55 +0530

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -1,18 +1,24 @@
 let { WAMessageProto } = require('@adiwajshing/baileys')
-let handler = async (m, { command, usedPrefix, text }) => {
+
+let handler = async (m, { conn, command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
-    let which = command.replace(/add/i, '')
-    if (!m.quoted) throw 'reply to the message!'
-    if (!text) throw `use *${usedPrefix}list${which}* to see the list`
-    let msgs = global.db.data.msgs
-    if (text in msgs) throw `'${text}' registered in the message list`
+    let which = command.replace(/\+|add/i, '')
+    if (!m.quoted) throw 'reply to the message!''
+    if (!text) return conn.sendButton(m.chat, `Uhm.. teksnya mana?\n\nContoh:\n${usedPrefix + command} tes`, '© wabot-aq', 'Daftar Pesan', `${usedPrefix}list${which}`, m)
+    let msgs = db.data.msgs
+    if (text in msgs) return conn.sendButton(m.chat, `'${text}' telah terdaftar, gunakan nama lain!`, '© wabot-aq', 'Daftar Pesan', `${usedPrefix}list${which}`, m)
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
-    m.reply(`Successfully added message in message list as '${text}'
+    if (db.data.chats[m.chat].getmsg) return m.reply(`Berhasil menambahkan pesan dengan nama '${text}'
     
-Access with ${usedPrefix}get${which} ${text}`)
+Akses dengan mengetik '${text}'`)
+    else return conn.sendButton(m.chat, `Berhasil menambahkan pesan dengan nama '${text}'
+
+Akses dengan ${usedPrefix}get${which} ${text}
+
+Jika Getmsg diaktifkan maka tidak perlu lagi mengetik *${usedPrefix}get${which}*`, '© wabot-aq', 'Aktifkan', '.1 getmsg', m)
 }
-handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker', 'gif'].map(v => 'add' + v + ' <teks>')
+handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'add' + v + ' <teks>')
 handler.tags = ['database']
-handler.command = /^add(vn|msg|video|audio|img|sticker|gif)$/
+handler.command = /^(\+|add)(vn|msg|video|audio|img|stic?ker|gif)$/
 
 module.exports = handler
