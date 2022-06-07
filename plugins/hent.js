cc0fe39b68c54ef6ae934182a613f12dc22ae0d5
sumitkant9536 <104259851+sumitkant9536@users.noreply.github.com>
2022-06-07 17:27:22 +0530

    Update hent.js

--- plugins/hent.js
+++ plugins/hent.js
@@ -1,20 +1,17 @@
 let fetch = require('node-fetch')
+
 let handler = async (m, { conn, usedPrefix, command }) => {
-     m.reply('tunggu...')
-  let res = await fetch('https://hmtai.herokuapp.com/nsfw/${command}')
-  let json = await res.json()
-  if (!json.url) throw json
-  await conn.sendButtonImg(m.chat, json.url, 'sagne sama kartun telanjang?', 'random ${command}', 'Lagi banh', '${usedPrefix + command}', m, false)
+	let res = await fetch(API('https://hmtai.herokuapp.com', '/nsfw/' + command, {}))
+	if (!res.ok) throw `${res.status} ${res.statusText}`
+	let json = await res.json()
+	if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'sgne sama kartun ?', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['ass', 'bdsm', 'blowjob', 'boobjob', 'cum', 'creampie', 'cuckold', 'ero', 'elves', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'incest', 'masturbation', 'pantsu', 'orgy', 'tentacles', 'thighs', 'uniform', 'vagina', 'yuri' ]
-handler.tags = ['dewasa']
-handler.command = /^(ass1|bdsm|blowjob|boobjob|cum|creampie|cuckold|ero|elves|femdom|foot|gangbang|glasses|hentai|incest|masturbation|pantsu|orgy|tentacles|thighs|uniform|vagina|yuri)$/i
+handler.tags = ['nsfw']
+handler.command = /^(ass|bdsm|blowjob|boobjob|cum|creampie|cuckold|ero|elves|femdom|foot|gangbang|glasses|hentai|incest|masturbation|pantsu|orgy|tentacles|thighs|uniform|vagina|yuri)$/i
 
-handler.private = false
-handler.limit = 2
+handler.premium = true
 handler.register = true
-handler.nsfw = false
 
 module.exports = handler
-
-//by lui
